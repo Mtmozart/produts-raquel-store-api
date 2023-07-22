d965af79cddff21313c6ab6feefa84e9957af305
Mtmozart <mmsnborges@gmail.com>
2023-07-22 19:16:07 -0300

    user

---
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { CreateUserUseCase } from './CreateUserUseCase';
+export class CreateUserController {
+  constructor(private CreateUserUserUseCase: CreateUserUseCase) {}
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    try{
+    await this.CreateUserUserUseCase.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.status(201).send()
+  }
+  catch{
+
+  }
+}
